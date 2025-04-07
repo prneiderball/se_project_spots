2d2f30babb855750ad8d7b1884ecf57aed623771
P.R. Neider-Ball <phillrballn@icloud.com>
2025-04-07 09:32:28 -0500

    Update Api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -25,6 +25,19 @@ class Api {
         })
         .catch(err => this.handleError(err));
     }
+
+    editProfileAvatar({ avatar }) {
+      return fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: "PATCH",
+        headers: { ...this._headers, authorization: this._headers.authorization, "Content-Type": "application/json" },
+        body: JSON.stringify({ avatar })
+      })
+        .then(res => {
+          if (res.ok) return res.json();
+          return Promise.reject(new Error(`Error: ${res.status}`));
+        })
+        .catch(err => this.handleError(err));
+    }
   
     getUserAndCards() {
       return Promise.all([this.getUserInfo(), this.getInitialCards()])
